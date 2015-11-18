7ffdff414f09603d77d93e81dbe74b920d926dab
BDJarko <tokken30@yahoo.com>
2015-11-18 16:00:59 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegex = /^\/MHH Bot/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/;
-      botRegexRu = /^\/rules/; botRegexYu = /^\/youtube/i; botRegexSd = /^\/schedule/;
+      botRegexRu = /^\/rules/; botRegexYu = /^\/youtube/i; botRegexSd = /^\/schedule/; botRegexTr = /^\/trades/;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -98,6 +98,11 @@ function respond() {
   else if(request.text && botRegexSd.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://daddyleagues.com/mhh/schedules");
+    this.res.end();
+  } 
+  else if(request.text && botRegexTr.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://www.daddyleagues.com/mhh/forum/forum/5074");
     this.res.end();
   }
   else {
